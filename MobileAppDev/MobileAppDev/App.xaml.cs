7c79cc9418ed49635fbd8fc481d0a3f31713c6c5
Chaz Goddard <mrcjmgoddard@outlook.com>
2022-06-13 15:20:05 +0100

    Add project files.

---
+++ MobileAppDev/MobileAppDev/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+
+namespace MobileAppDev
+{
+    public partial class App : Application
+    {
+        //initialises the folderpath string used to store and recal notes.
+        public static string Folderpath { get; private set; } 
+        public App()
+        {
+            InitializeComponent();
+            Folderpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MobileAppDev/MobileAppDev/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+using MobileAppDev.Views;
+
+namespace MobileAppDev
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            //As the Note Entry Page isn't needed by users until they wish to make a note the route the app needs is registered in here
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(NoteEntryPage), typeof(NoteEntryPage));
+        }
+    }
+}
